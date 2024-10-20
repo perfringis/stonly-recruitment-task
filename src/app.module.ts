0f4fe2c4784b6296072cc58b66679d54a6a5b611
perfringis <kopko.mateusz@gmail.com>
2024-10-20 12:53:57 +0200

    add db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRESQL_DB_HOST || 'localhost',
+      port: process.env.POSTGRESQL_DB_PORT
+        ? parseInt(process.env.POSTGRESQL_DB_PORT, 10)
+        : 5432,
+      username: process.env.POSTGRESQL_DB_USERNAME,
+      password: process.env.POSTGRESQL_DB_PASSWORD,
+      database: process.env.POSTGRESQL_DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: true,
+      entities: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
